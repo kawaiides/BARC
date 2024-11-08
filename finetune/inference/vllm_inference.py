0e4e753098c819443570d79a92093b0050455e78
kawaii <v3rykawaiides@gmail.com>
2024-11-08 23:27:35 +0530

    Update vllm_inference.py

--- finetune/inference/vllm_inference.py
+++ finetune/inference/vllm_inference.py
@@ -119,8 +119,8 @@ for d in tqdm(data):
 
     if not aggregate_outputs:
         breakpoint()
-    else:
-        #print(aggregate_outputs[0])
+    # else:
+    #     print(aggregate_outputs[0])
 
 
     # Print the outputs.
